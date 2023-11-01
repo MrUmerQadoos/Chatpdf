9622dd166cafd3a07a78cd4d590c599fc1bcd46e
MrUmerQadoos <umerqadoos74@gmail.com>
2023-11-01 09:46:27 +0500

    commit message

---
+++ src/app/api/create-chat/route.ts
@@ -0,0 +1,22 @@
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
+
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+// /api/create-chat
+export async function POST(req: Request, res: Response) {
+
+    try {
+        const body = await req.json();
+        const { file_key, file_name } = body;
+        console.log(file_key, file_name);
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { error: "internal server error" },
+            { status: 500 }
+        );
+    }
+}
